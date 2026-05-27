f9a707c1498b01fe5ed77e70c376dac0db9f963d
shambhu patil <shambhupatil@shambhus-Mac.local>
2026-05-27 16:09:28 +0530

    2nd try

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/shambhu-portfolio/',
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
 })
